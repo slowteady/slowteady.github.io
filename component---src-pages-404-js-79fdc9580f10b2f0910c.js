e7fa5c4f6c7553ed09c5a6457d3d13d410a2202a
slowteady <slowteady@users.noreply.github.com>
2023-03-09 02:27:28 +0000

    deployed via Gatsby Publish Action 🎩 for e9a6d348c1a30f57aa69db49651d688e1ecb3a6f

---
+++ component---src-pages-404-js-79fdc9580f10b2f0910c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkslowteady_io=self.webpackChunkslowteady_io||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(4218),a=n(7743);t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-79fdc9580f10b2f0910c.js.map
